eaeca6c3aa3a62201bb6905b0bee40008d1fbcfd
Aleksey Filippov <sarum9in@gmail.com>
2013-12-28 19:22:03 +0400

    Add derive(), crypt() and decrypt()

--- CryptEngine.cpp
+++ CryptEngine.cpp
@@ -1,8 +1,56 @@
 #include "CryptEngine.hpp"
 
+#include <QCryptographicHash>
+
 CryptEngine::CryptEngine(QObject *parent):
     QObject(parent)
 {
+}
+
+static QByteArray derive(const QString &derivationFunction, const QString &text, const int maxSize=0)
+{
+    QCryptographicHash::Algorithm algo;
+    if (derivationFunction == "MD5")
+        algo = QCryptographicHash::Md5;
+    else if (derivationFunction == "SHA-256")
+        algo = QCryptographicHash::Sha256;
+    else if (derivationFunction == "SHA-512")
+        algo = QCryptographicHash::Sha512;
+    else
+        algo = QCryptographicHash::Sha512;
+    QByteArray derived = QCryptographicHash::hash(text.toUtf8(), algo);
+
+    if (maxSize)
+    {
+        QByteArray result(maxSize, '\0');
+        for (int i = 0; i < derived.size(); ++i)
+        {
+            // ^= is not supported
+            const int targetPos = i % result.size();
+            result[targetPos] = result[targetPos] ^ derived[i];
+        }
+        return result;
+    }
+    else
+    {
+        return derived;
+    }
+}
+
+static QByteArray crypt(const QByteArray &key,
+                        const QString &modeOfOperation,
+                        const QByteArray &iv,
+                        const QByteArray &text)
+{
+    return text;
+}
+
+static QByteArray decrypt(const QByteArray &key,
+                          const QString &modeOfOperation,
+                          const QByteArray &iv,
+                          const QByteArray &cipherText)
+{
+    return cipherText;
 }
 
 void CryptEngine::crypt(const QString &keyDerivationFunction,
@@ -11,7 +59,13 @@ void CryptEngine::crypt(const QString &keyDerivationFunction,
                         const QString &iv,
                         const QString &text)
 {
-    emit crypted("[" + text + "]");
+    const QByteArray cipherText = ::crypt(
+        derive(keyDerivationFunction, key),
+        modeOfOperation,
+        derive(keyDerivationFunction, iv),
+        text.toUtf8()
+    );
+    emit crypted(QString(cipherText.toBase64()));
 }
 
 void CryptEngine::decrypt(const QString &keyDerivationFunction,
@@ -20,8 +74,11 @@ void CryptEngine::decrypt(const QString &keyDerivationFunction,
                           const QString &iv,
                           const QString &cipherText)
 {
-    if (!cipherText.isEmpty() && cipherText.at(0) == '[' && cipherText.at(cipherText.size() - 1) == ']')
-        emit decrypted(cipherText.mid(1, cipherText.size() - 2));
-    else
-        emit decrypted("ERROR");
+    const QByteArray text = ::decrypt(
+        derive(keyDerivationFunction, key),
+        modeOfOperation,
+        derive(keyDerivationFunction, iv),
+        QByteArray::fromBase64(cipherText.toUtf8())
+    );
+    emit decrypted(QString::fromUtf8(text));
 }
